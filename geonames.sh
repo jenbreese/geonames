05bfef60c71bf7a554a37157a22667e7ec36592f
Scott Stafford <scott.stafford@marklogic.com>
2014-08-13 19:43:21 -0700

    adding headers into SC.zip

--- geonames.sh
+++ geonames.sh
@@ -1,7 +1,17 @@
 mkdir data
 cd data
-curl -O http://download.geonames.org/export/dump/countryInfo.txt
-curl -O http://download.geonames.org/export/dump/admin1CodesASCII.txt
 
+#download countryInfo and format
+curl -O http://download.geonames.org/export/dump/countryInfo.txt
 sed -n 51,`cat countryInfo.txt | wc -l`p countryInfo.txt > countryInfo.csv
 sed -i '1s/#//g' countryInfo.csv
+
+curl -O http://download.geonames.org/export/dump/admin1CodesASCII.txt
+
+mkdir SC
+cd SC
+curl -O http://download.geonames.org/export/dump/SC.zip
+unzip SC.zip
+cd ../..
+./headers.sh SC
+
--- headers.sh
+++ headers.sh
@@ -1,1 +1,3 @@
-sed '1 i\geonameid	name	asciiname	alternatenames	latitude	longitude	feature-class	feature-code	country-code	cc2	admin1-code	admin2-code	admin3-code	admin4-code	population	elevation	dem	timezone	modification-date' US/US.txt > temp.txt && mv temp.txt US/US.txt 
+COUNTRY=$1
+
+sed '1 i\geonameid	name	asciiname	alternatenames	latitude	longitude	feature-class	feature-code	country-code	cc2	admin1-code	admin2-code	admin3-code	admin4-code	population	elevation	dem	timezone	modification-date' data/$COUNTRY/$COUNTRY.txt > temp.txt && mv temp.txt data/$COUNTRY/$COUNTRY.txt 
