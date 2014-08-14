598a84036c99f0390de85b057c58fad90b91f38d
Scott Stafford <scott.stafford@marklogic.com>
2014-08-14 09:46:27 -0700

    adding collection and directory

--- mlcp/ingest-geonames.sh
+++ mlcp/ingest-geonames.sh
@@ -7,10 +7,10 @@ mlcp.sh IMPORT \
   -password admin \
   -input_file_path ../data/$COUNTRY/$COUNTRY.txt \
   -input_file_type delimited_text \
-  -output_collections "geonames" \
+  -output_collections "geonames,$COUNTRY" \
   -delimited_root_name feature \
   -namespace http://geonames.org \
   -options_file options.txt \
-  -output_uri_prefix "/geonames/" \
+  -output_uri_prefix "/geonames/$COUNTRY/" \
   -transform_module "/xqy/lib/lib-geonames.xqy" \
   -transform_namespace "http://geonames.org" 
