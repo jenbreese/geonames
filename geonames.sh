567656ca301121b7ac5b5be802687aca284ecb24
Scott Stafford <scott.stafford@marklogic.com>
2014-08-13 19:20:38 -0700

    fixing geonames ingest for country codes

--- geonames.sh
+++ geonames.sh
@@ -2,3 +2,6 @@ mkdir data
 cd data
 curl -O http://download.geonames.org/export/dump/countryInfo.txt
 curl -O http://download.geonames.org/export/dump/admin1CodesASCII.txt
+
+sed -n 51,`cat countryInfo.txt | wc -l`p countryInfo.txt > countryInfo.csv
+sed -i '1s/#//g' countryInfo.csv
--- mlcp/ingest-country-codes.sh
+++ mlcp/ingest-country-codes.sh
@@ -3,11 +3,11 @@ mlcp.sh IMPORT \
   -port 8011 \
   -username admin \
   -password admin \
-  -input_file_path out.txt \
+  -input_file_path ../data/countryInfo.csv \
   -input_file_type delimited_text \
-  -output_collections "geonames,countryCodes" \
-  -delimited_root_name feature \
-  -namespace http://geonames.org \
+  -output_collections "countryInfo" \
+  -delimited_root_name country \
+  -namespace http://geonames.org/countryInfo \
   -options_file options.txt \
-  -output_uri_prefix "/countryCodes/"
+  -output_uri_prefix "/countryInfo/"
 
