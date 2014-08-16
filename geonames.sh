49c8026a9b3ad819902818d84c600345e6f4b267
Scott Stafford <scott.stafford@marklogic.com>
2014-08-16 10:56:26 -0700

    changed up geonames script

--- geonames.sh
+++ geonames.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+COUNTRY=$1
+
+function loadCountry {
+  mkdir $1
+  cd $1
+  curl -O http://download.geonames.org/export/dump/$1.zip
+  unzip $1.zip
+  rm -f $1.zip
+  rm -f readme.txt
+  cd ../..
+  ./headers.sh $1
+  cd data
+}
+
 mkdir data
 cd data
 
-for country in `cut -f 1 countryInfo.csv`; do
-  mkdir $country
-  cd $country
-  curl -O http://download.geonames.org/export/dump/$country.zip
-  unzip $country.zip
-  cd ../..
-  ./headers.sh $country
-  cd data
-done
+if [ "$COUNTRY" == "all" ]; then 
+  for country in `cut -f 1 countryInfo.csv`; do
+    loadCountry $country
+  done
+else
+  loadCountry $COUNTRY
+fi
+
 
