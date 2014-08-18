41daa9c17d7c172f7bc1e05a1e3edd4baa8d71d0
Scott Stafford <scott.stafford@marklogic.com>
2014-08-17 19:22:01 -0700

    fixing file reference

--- load-geonames.sh
+++ load-geonames.sh
@@ -5,7 +5,7 @@ COUNTRY=$1
 cd mlcp
 
 if [ "$COUNTRY" == "all" ]; then 
-  for country in `cut -f 1 countryInfo.csv`; do
+  for country in `cut -f 1 ../data/countryInfo.csv`; do
     ./ingest-geonames.sh $country
   done
 else
