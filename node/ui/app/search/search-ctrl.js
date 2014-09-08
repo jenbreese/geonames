31f1db2b0a18461fbd2d3d3f8a58082f6a1e3a1f
Bruce Holt <Bruce.Holt@marklogic.com>
2014-09-08 15:47:11 -0600

    adding links to geonames

--- node/ui/app/search/search-ctrl.js
+++ node/ui/app/search/search-ctrl.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('geonames.search')
-    .controller('SearchCtrl', ['$scope', 'MLRest', '$location', '$sce', function ($scope, mlRest, $location, $sce) {
+    .controller('SearchCtrl', ['$scope', 'MLRest', '$location', '$sce', '$filter', function ($scope, mlRest, $location, $sce, $filter) {
       var model = {
         selected: [],
         text: '',
@@ -17,9 +17,9 @@
 
       (function init() {
         //mlRest.enrich(model.text, model.code).then(updateSearchResults);
-        mlRest.countries("true").then(function(data) {
+        mlRest.countries('true').then(function(data) {
           model.countries = data.countries.country;
-          model.countries.unshift({Country: "World", ISO: ""});
+          model.countries.unshift({Country: 'World', ISO: ''});
         });
       })();
 
@@ -31,6 +31,24 @@
         },
         renderQuery: function() {
           return model.search.summary ? $sce.trustAsHtml(model.search.summary.query) : '';
+        },
+        getCountryByCode: function(code) {
+          var found = $filter('filter')(model.countries, {ISO: code});
+          if (found.length) {
+            return found[0].Country;
+          } else { return ''}
+        },
+        getMain: function(names) {
+          var found = $filter('filter')(names, {tag: "main"});
+          if (found.length) {
+            return found[0]._value;
+          } else {return ''}
+        },
+        getCountryGeonamesIdByCode: function(code) {
+          var found = $filter('filter')(model.countries, {ISO: code});
+          if (found.length) {
+            return found[0].geonameid;
+          } else { return ''}
         }
       });
 
